6e725a5f1e2161929922c0c8d00c17e6f8fb4b8d
Pahrul Irfan <irfan@universitasbumigora.ac.id>
2023-10-11 11:31:48 +0800

    tampil data

---
+++ Penulis/tampil.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Penulis</title>
+</head>
+
+<body>
+
+    <h2 style="text-align:center">Data Penulis</h2>
+
+    <br>
+
+    <a href="tambah.php">Tambah Data</a> 
+
+    <br>
+
+    <table border="1" width="70%" align="center">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Alamat</th>
+                <th>HP</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                include('../setting.php');
+                $no = 1;
+                $query = "SELECT * FROM penulis";
+                $result = mysqli_query($link, $query);
+                while ($row = mysqli_fetch_object($result)) {
+            ?>
+                
+                <tr>
+                    <td> <?php echo $no++; ?> </td>
+                    <td> <?= $row->nama; ?> </td>
+                    <td> <?= $row->alamat; ?> </td>
+                    <td> <?= $row->hp; ?> </td>
+                    <td>
+                        Update | Delete
+                    </td>
+                </tr>
+
+            <?php } ?>
+
+        </tbody>
+    </table>
+
+</body>
+
+</html>
