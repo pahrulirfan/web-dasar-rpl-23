fa521f3e96f25a98d4ab1a12a6d8933df4441697
Pahrul Irfan <irfan@universitasbumigora.ac.id>
2023-10-11 11:32:03 +0800

    form tambah

---
+++ Penulis/tambah.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Penulis</title>
+</head>
+<body>
+    
+    <h2 style="text-align:center">Tambah Penulis</h2>
+
+    <form action="" method="post">
+
+        <label>Nama : </label>
+        <input type="text" name="txtnama"> <br>
+
+        <label>Alamat : </label>
+        <input type="text" name="txtalamat"> <br>
+
+        <label>Nomor HP : </label>
+        <input type="text" name="txthp"> <br>
+
+        <input type="submit" value="Simpan" name="btnsimpan">
+        <a href="tampil.php">Batal</a>
+
+    </form>
+
+</body>
+</html>
