b9388440ea41ba0dd212c3fbd51da9ca795d4b67
Pahrul Irfan <irfan@universitasbumigora.ac.id>
2023-11-06 08:53:04 +0800

    proses tambah data

--- Penulis/tambah.php
+++ Penulis/tambah.php
@@ -1,14 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Penulis</title>
 </head>
+
 <body>
-    
+
     <h2 style="text-align:center">Tambah Penulis</h2>
 
+    <?php
+    if (isset($_POST["btnsimpan"])) {
+
+        require("../setting.php");
+
+        $inputnama = htmlspecialchars($_POST["txtnama"]);
+        $inputalamat = htmlspecialchars($_POST["txtalamat"]);
+        $inputhp = htmlspecialchars($_POST["txthp"]);
+
+        $query = "INSERT INTO penulis VALUES (NULL, '$inputnama', '$inputalamat', '$inputhp')";
+
+        $result = mysqli_query($link, $query);
+
+        if ($result) {
+            header("location: tampil.php");
+        } else {
+            echo "Data Gagal disimpan !";
+        }
+
+    }
+
+    ?>
     <form action="" method="post">
 
         <label>Nama : </label>
@@ -26,4 +50,5 @@
     </form>
 
 </body>
+
 </html>
